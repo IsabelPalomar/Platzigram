bf8d37b7363cf831ddec1324cc8621148e98c8be
Isabel Palomar <isabel.palomarb@gmail.com>
2016-10-09 15:50:18 -0400

    Obtener el thumbnail

--- app/src/main/java/com/platzi/platzigram/view/fragment/NewPostFragment.java
+++ app/src/main/java/com/platzi/platzigram/view/fragment/NewPostFragment.java
@@ -2,6 +2,7 @@ package com.platzi.platzigram.view.fragment;
 
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
@@ -13,12 +14,16 @@ import android.widget.ImageView;
 
 import com.platzi.platzigram.R;
 
+import static android.app.Activity.RESULT_OK;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NewPostFragment extends Fragment {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    ImageView ivPicture;
 
     public NewPostFragment() {
         // Required empty public constructor
@@ -32,6 +37,7 @@ public class NewPostFragment extends Fragment {
         View view =  inflater.inflate(R.layout.fragment_new_post, container, false);
 
         Button btnTakePicture =  (Button) view.findViewById(R.id.btnTakePicture);
+        ivPicture = (ImageView) view.findViewById(R.id.ivPicture);
 
         btnTakePicture.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +47,15 @@ public class NewPostFragment extends Fragment {
         });
 
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ivPicture.setImageBitmap(imageBitmap);
+        }
     }
 
     private void takePicture() {
