acd3403118033336df64f6afd20ee45c57c63063
Isabel Palomar <isabel.palomarb@gmail.com>
2016-10-09 14:12:06 -0400

    Tomando una foto

--- app/src/main/java/com/platzi/platzigram/view/fragment/NewPostFragment.java
+++ app/src/main/java/com/platzi/platzigram/view/fragment/NewPostFragment.java
@@ -1,11 +1,15 @@
 package com.platzi.platzigram.view.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.platzi.platzigram.R;
 
@@ -14,6 +18,7 @@ import com.platzi.platzigram.R;
  */
 public class NewPostFragment extends Fragment {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     public NewPostFragment() {
         // Required empty public constructor
@@ -24,7 +29,25 @@ public class NewPostFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_post, container, false);
+        View view =  inflater.inflate(R.layout.fragment_new_post, container, false);
+
+        Button btnTakePicture =  (Button) view.findViewById(R.id.btnTakePicture);
+
+        btnTakePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture();
+            }
+        });
+
+        return view;
+    }
+
+    private void takePicture() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null){
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 
 }
